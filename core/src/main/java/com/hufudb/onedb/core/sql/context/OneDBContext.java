ed0b0a27c75961886456dc35e6ef4ce9a8a65458
pxc <xc_pan@foxmail.com>
2022-05-06 22:57:12 +0800

    clean code

--- core/src/main/java/com/hufudb/onedb/core/sql/context/OneDBContext.java
+++ core/src/main/java/com/hufudb/onedb/core/sql/context/OneDBContext.java
@@ -11,7 +11,6 @@ import com.hufudb.onedb.core.implementor.utils.OneDBJoinInfo;
 import com.hufudb.onedb.core.rewriter.OneDBRewriter;
 import com.hufudb.onedb.core.sql.expression.OneDBExpression;
 import com.hufudb.onedb.core.sql.rel.OneDBOrder;
-import com.hufudb.onedb.core.table.OneDBTableInfo;
 import com.hufudb.onedb.rpc.OneDBCommon.QueryContextProto;
 import com.hufudb.onedb.rpc.OneDBCommon.TaskInfoProto;
 import java.util.List;
