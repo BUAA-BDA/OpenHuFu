90fcd595855ba2689599d92864eefd6f16c4dcb1
pxc <xc_pan@foxmail.com>
2022-05-20 11:43:45 +0800

    fix setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,16 @@
 import setuptools
 
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setuptools.setup(
     name="pyonedb",
-    version="1.0.0"
+    version="1.0.0",
     description="OneDB Python API",
     author="pxc",
     author_email="xc_pan@foxmail.com",
     packages=['pyonedb'],
-    python_requires=">=3.7"
-    install_requires=['jnius==1.4.1']
+    python_requires=">=3.7",
+    install_requires=['pyjnius==1.4.1'],
+    test_requires=['pytest==7.1.2']
 )
