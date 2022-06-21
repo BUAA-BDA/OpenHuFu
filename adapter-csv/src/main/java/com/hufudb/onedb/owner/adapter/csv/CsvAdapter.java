372b1f7515dcc4338ec06b535844f9d8886bb92e
pxc <xc_pan@foxmail.com>
2022-06-21 16:06:24 +0800

    update heterogeneous datasource docker environment

--- adapter-csv/src/main/java/com/hufudb/onedb/owner/adapter/csv/CsvAdapter.java
+++ adapter-csv/src/main/java/com/hufudb/onedb/owner/adapter/csv/CsvAdapter.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
