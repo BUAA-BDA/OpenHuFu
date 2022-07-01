82464fc8b42e6a89552723621cfe85de9330363e
pxc <xc_pan@foxmail.com>
2022-07-01 19:05:34 +0800

    add benchmark

---
+++ docker/benchmark/download_data.sh
@@ -0,0 +1,16 @@
+download() {
+  echo "start download 10^$1 scale dataset"
+  wget http://hufudb.com/data/$1.tar.gz
+  rm -rf database/data
+  tar -xvzf $1.tar.gz
+  mv $1 database/data
+}
+
+if [ $# -eq 1 ]
+then
+  download $1
+else
+  echo "please enter the scale of data to be downloaded"
+  echo "eg"
+  echo "./download_data.sh 3"
+fi
