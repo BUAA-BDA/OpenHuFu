6b1a08125dd9f5fa57000241a04428bd1b6c2f46
牛易明 <neumy@qq.com>
2022-07-08 16:49:46 +0800

    [feat] : 修改了Point数据类型的字符串转化，现在用字符串'Point(x, y)'代表点(x,y)

--- data/src/main/java/com/hufudb/onedb/data/storage/utils/PointUtils.java
+++ data/src/main/java/com/hufudb/onedb/data/storage/utils/PointUtils.java
@@ -3,13 +3,14 @@ package com.hufudb.onedb.data.storage.utils;
 import com.hufudb.onedb.data.storage.Point;
 
 public class PointUtils {
-  public PointUtils() {}
+  public PointUtils() {
+  }
 
   /**
    * convert com.hufudb.onedb.data.storage.Point to String
    */
   public String pointToStr(Point p) {
-    return String.valueOf(p.getX()) + " " +String.valueOf(p.getY());
+    return "Point(" + String.valueOf(p.getX()) + " " + String.valueOf(p.getY()) + ")";
   }
 
   /**
@@ -17,12 +18,12 @@ public class PointUtils {
    */
   public Point strToPoint(String s) {
     try {
-      String[] newStr = s.split("\\s+");
+      String tmp = s.substring(6, s.length()-1);
+      String[] newStr = tmp.split("\\s+");
       return new Point(Double.valueOf(newStr[0]), Double.valueOf(newStr[1]));
-    } catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
       return null;
     }
   }
 }
-
