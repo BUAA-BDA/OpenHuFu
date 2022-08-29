3673037338ebed23ee9cc2705bf8262a30196bb9
pxc <xc_pan@foxmail.com>
2022-08-29 19:04:09 +0800

    simplify backend configuration and add backend demo

--- release/demo/backend.sh
+++ release/demo/backend.sh
@@ -1,13 +1,13 @@
 start_owner() {
   echo "starting server $1..."
-  nohup java -jar ../bin/backend.jar --spring.config.location=../config/server$1.properties > log/$1.log &
+  nohup java -jar ../bin/backend.jar --spring.config.location=./config/server$1.properties > log/$1.log &
   echo $! >> log/pid_$1
   echo "server$1 backend start"
 }
 
 start_user() {
   echo "starting user..."
-  nohup java -jar ../bin/backend.jar --spring.config.location=../config/user.properties > log/u.log &
+  nohup java -jar ../bin/backend.jar --spring.config.location=./config/user.properties > log/u.log &
   echo $! >> log/pid_u
   echo "user backend start"
 }
@@ -18,12 +18,14 @@ stop() {
   rm log/pid_$1
 }
 
+usage() {
+  echo "./backend.sh [user | owner | stop]"
+}
+
 export ONEDB_ROOT=$PWD/..
 mkdir -p log
-if [ $# -eq 0 ]
-then
-  start_user
-elif [ "$1" = "owner" ]
+
+if [ "$1" = "owner" ]
 then
   start_owner "3"
   start_owner "2"
@@ -38,5 +40,5 @@ then
   stop "3"
   stop "u"
 else
-  start_owner $1
+  usage
 fi
