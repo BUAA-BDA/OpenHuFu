41b1e4d861fe6443389823bef6a9f433611a29c0
pxc <xc_pan@foxmail.com>
2022-08-31 15:30:33 +0800

    fix pom dependency for hive-adapter

--- adapter/adapter-hive/src/test/java/com/hufudb/onedb/owner/adapter/hive/HiveAdapterTest.java
+++ adapter/adapter-hive/src/test/java/com/hufudb/onedb/owner/adapter/hive/HiveAdapterTest.java
@@ -1,9 +1,14 @@
 package com.hufudb.onedb.owner.adapter.hive;
 
 import java.sql.*;
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class HiveAdapterTest {
-  public static void main(String[] args) {
+
+  @Ignore
+  @Test
+  public void testHive() {
     String driverName = "org.apache.hive.jdbc.HiveDriver";
 
     try {
@@ -17,8 +22,7 @@ public class HiveAdapterTest {
       Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000", "", "");
       Statement stmt = con.createStatement();
 
-
-      String sql = "select * from pokes";
+      String sql = "select * from pokes limit 10";
       System.out.println("Running: " + sql);
       ResultSet res = stmt.executeQuery(sql);
       while (res.next()) {
@@ -29,5 +33,3 @@ public class HiveAdapterTest {
     }
   }
 }
-
-
