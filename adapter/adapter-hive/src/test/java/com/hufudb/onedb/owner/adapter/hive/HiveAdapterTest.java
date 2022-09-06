623d7edb57746c2f226df02f55b8b786617b1dc1
WantongChen <2044648583@qq.com>
2022-09-06 12:58:03 +0800

    fix HiveAdapterTest

--- adapter/adapter-hive/src/test/java/com/hufudb/onedb/owner/adapter/hive/HiveAdapterTest.java
+++ adapter/adapter-hive/src/test/java/com/hufudb/onedb/owner/adapter/hive/HiveAdapterTest.java
@@ -2,10 +2,14 @@ package com.hufudb.onedb.owner.adapter.hive;
 
 import java.sql.*;
 
-import com.hufudb.onedb.data.schema.TableSchema;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * Before testing, start docker in docker/hereto/hive
+ */
 public class HiveAdapterTest {
 
   @Ignore
@@ -24,13 +28,7 @@ public class HiveAdapterTest {
       Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "", "");
       Statement stmt = con.createStatement();
 
-      String sql = "select * from student";
-      System.out.println("Running: " + sql);
-      ResultSet res = stmt.executeQuery(sql);
-      while (res.next()) {
-        System.out.println(String.valueOf(res.getString(1)));
-      }
-
+      //test metadata, expected result see init.sql
       DatabaseMetaData meta = con.getMetaData();
       ResultSet rs = meta.getTables("default", null, "%", new String[] {"TABLE"});
       while (rs.next()) {
@@ -45,6 +43,62 @@ public class HiveAdapterTest {
         rc.close();
       }
       rs.close();
+
+      //test query
+      String sql = "select * from student";
+      System.out.println("Running: " + sql);
+      ResultSet res = stmt.executeQuery(sql);
+      int count = 0;
+      while (res.next()) {
+        System.out.println(String.valueOf(res.getString(1)));
+        count++;
+      }
+      assertEquals(3, count);
+      res.close();
+
+      sql = "select * from student where score < 88.5";
+      System.out.println("Running: " + sql);
+      res = stmt.executeQuery(sql);
+      res.next();
+      assertEquals("helen", res.getString(1));
+      assertEquals(75.4, res.getDouble(3), 0.001);
+      assertFalse(res.next());
+      res.close();
+
+      sql = "select SUM(age) from student";
+      System.out.println("Running: " + sql);
+      res = stmt.executeQuery(sql);
+      res.next();
+      assertEquals(66, res.getInt(1));
+      assertFalse(res.next());
+      res.close();
+
+      sql = "select * from time";
+      System.out.println("Running: " + sql);
+      res = stmt.executeQuery(sql);
+      res.next();
+      assertEquals(1, res.getInt(1));
+      assertEquals(Date.valueOf("2018-06-01"), res.getDate(2));
+      assertEquals(Timestamp.valueOf("2018-06-01 10:14:45"), res.getTimestamp(3));
+      count = 1;
+      while (res.next()) {
+        count ++;
+        System.out.println(String.valueOf(res.getDate(2)));
+      }
+      assertEquals(6, count);
+      res.close();
+
+      sql = "select id from time where test_date > '2019-06-01' and test_timestamp < '2020-05-01 05:15:55'";
+      System.out.println("Running: " + sql);
+      res = stmt.executeQuery(sql);
+      res.next();
+      assertEquals(3, res.getInt(1));
+      res.next();
+      assertEquals(4, res.getInt(1));
+      res.next();
+      assertFalse(res.next());
+      res.close();
+
     } catch (SQLException e) {
       e.printStackTrace();
     }
--- docker/hetero/hive/init/init.sql
+++ docker/hetero/hive/init/init.sql
@@ -1,8 +1,33 @@
 DROP TABLE if exists student;
 CREATE TABLE student (name STRING,
                       age INT,
-                      score INT,
+                      score DOUBLE,
                       dept_name STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
 
 LOAD DATA LOCAL INPATH '/opt/hive/init/data.txt' OVERWRITE INTO TABLE student;
+
+DROP TABLE if exists time;
+CREATE TABLE time (id INT,
+                    test_date DATE,
+                    test_timestamp TIMESTAMP)
+    ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INPATH '/opt/hive/init/data2.txt' OVERWRITE INTO TABLE time;
+
+DROP TABLE if exists types;
+CREATE TABLE types (a TINYINT,
+                   b SMALLINT,
+                   c INT,
+                   d BIGINT,
+                   e BOOLEAN,
+                   f FLOAT,
+                   g DOUBLE,
+                   h DECIMAL,
+                   i STRING,
+                   j CHAR(4),
+                   k VARCHAR(16),
+                   l TIMESTAMP,
+                   n DATE,
+                   o BINARY,
+                   p INTEGER);
