cbf98c213064b8035bcab14dc0690f82157f4b9a
Qwtdgh <2775257495@qq.com>
2022-10-10 20:09:32 +0800

    add origin mysql databse & monitor user sqlQuery

---
+++ backend/src/main/java/com/hufudb/onedb/backend/controller/SqlRecordController.java
@@ -0,0 +1,39 @@
+package com.hufudb.onedb.backend.controller;
+
+
+import com.hufudb.onedb.backend.entity.SqlRecord;
+import com.hufudb.onedb.backend.service.SqlRecordService;
+import com.hufudb.onedb.backend.utils.Page;
+import com.hufudb.onedb.backend.utils.PageUtils;
+import com.hufudb.onedb.backend.utils.RecordRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  Front Controller
+ * </p>
+ *
+ * @author qlh
+ * @since 2022-10-21
+ */
+@RestController
+@RequestMapping("/sqlRecord")
+public class SqlRecordController {
+
+    @Autowired
+    SqlRecordService sqlRecordService;
+
+    @PostMapping("/query")
+    Page<SqlRecord> query(@RequestBody RecordRequest request) {
+        int pageId = request.pageId;
+        int pageSize = request.pageSize;
+        String context = request.context == null ? ".*" : request.context;
+        String status = request.status == null ? ".*" : request.status;
+        return PageUtils.getPage(()->sqlRecordService.selectRecord(context, status), pageId, pageSize);
+    }
+}
---
+++ backend/src/main/java/com/hufudb/onedb/backend/utils/RecordRequest.java
@@ -0,0 +1,8 @@
+package com.hufudb.onedb.backend.utils;
+
+public class RecordRequest {
+    public String context;
+    public String status;
+    public int pageId;
+    public int pageSize;
+}
