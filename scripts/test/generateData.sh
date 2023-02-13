dceb705140c4a41a1886227341b71c72b90cefe8
cirnoooo123 <1601169949@qq.com>
2023-02-13 21:58:49 +0800

    fix generateData.sh

--- scripts/test/generateData.sh
+++ scripts/test/generateData.sh
@@ -1,40 +1,40 @@
-bash makeDir.sh $1 $2
+bash ./scripts/test/makeDir.sh $1 $2
 ((totalSize = $2 * $3))
 echo "generating data, total size: $totalSize G"
-cd ..
+cd ./dataset/TPC-H\ V3.0.1/dbgen/
 ./dbgen -f -s $totalSize
-cd ./scripts
 
+pwd
 i=0
 while ((i < $2))
 do
 	echo "separating data, running for database$i"
 	((a = i * 150000 + 1))
 	((b = (i + 1) * 150000))
-	sed -n "$a,$b"p ../customer.tbl > $1/database$i/customer.tbl
+	sed -n "$a,$b"p ./customer.tbl | sed 's/.$//' > $1/database$i/customer.tbl
 
 	((a = i * 6000000 + 1))
 	((b = (i + 1) * 6000000))
-	sed -n "$a,$b"p ../lineitem.tbl > $1/database$i/lineitem.tbl
+	sed -n "$a,$b"p ./lineitem.tbl | sed 's/.$//' > $1/database$i/lineitem.tbl
 
 	((a = i * 6000000 + 1))
 	((b = (i + 1) * 6000000))
-	sed -n "$a,$b"p ../orders.tbl > $1/database$i/orders.tbl
+	sed -n "$a,$b"p ./orders.tbl | sed 's/.$//' > $1/database$i/orders.tbl
 
 	((a = i * 200000 + 1))
 	((b = (i + 1) * 200000))
-	sed -n "$a,$b"p ../part.tbl > $1/database$i/part.tbl
+	sed -n "$a,$b"p ./part.tbl | sed 's/.$//' > $1/database$i/part.tbl
 
 	((a = i * 800000 + 1))
 	((b = (i + 1) * 800000))
-	sed -n "$a,$b"p ../partsupp.tbl > $1/database$i/partsupp.tbl
+	sed -n "$a,$b"p ./partsupp.tbl | sed 's/.$//' > $1/database$i/partsupp.tbl
 
 	((a = i * 10000 + 1))
 	((b = (i + 1) * 10000))
-	sed -n "$a,$b"p ../supplier.tbl > $1/database$i/supplier.tbl
+	sed -n "$a,$b"p ./supplier.tbl | sed 's/.$//' > $1/database$i/supplier.tbl
 
-	cp ../nation.tbl $1/database$i
-	cp ../region.tbl $1/database$i
+	sed 's/.$//' ./nation.tbl > $1/database$i/nation.tbl
+	sed 's/.$//' ./region.tbl > $1/database$i/region.tbl
 	((i++))
 done
-rm ../*.tbl
+rm ./*.tbl
