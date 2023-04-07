985971518d881835eee4bddd043202d5cb49c431
wyx <2063959711@qq.com>
2023-04-07 11:29:29 +0800

    support multiparty cmp

--- mpc/src/main/java/com/hufudb/openhufu/mpc/lib/aby/MultiCmp.java
+++ mpc/src/main/java/com/hufudb/openhufu/mpc/lib/aby/MultiCmp.java
@@ -1,12 +1,9 @@
 package com.hufudb.openhufu.mpc.lib.aby;
 
-
-import java.util.ArrayList;
 import java.util.List;
 import com.google.common.collect.ImmutableList;
 import com.hufudb.openhufu.mpc.ProtocolException;
 import com.hufudb.openhufu.mpc.ProtocolExecutor;
-import com.hufudb.openhufu.mpc.ProtocolFactory;
 import com.hufudb.openhufu.mpc.ProtocolType;
 import com.hufudb.openhufu.mpc.RpcProtocolExecutor;
 import com.hufudb.openhufu.proto.OpenHuFuService.OwnerInfo;
@@ -14,63 +11,60 @@ import com.hufudb.openhufu.mpc.codec.OpenHuFuCodec;
 import com.hufudb.openhufu.proto.OpenHuFuData.ColumnType;
 import com.hufudb.openhufu.proto.OpenHuFuPlan.OperatorType;
 import com.hufudb.openhufu.rpc.Rpc;
-import com.hufudb.openhufu.rpc.utils.DataPacket;
-import com.hufudb.openhufu.rpc.utils.DataPacketHeader;
+import com.hufudb.openhufu.rpc.grpc.OpenHuFuRpc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MultiCmp {
-    private int me;
-    private int num;
-    private String address;
-    private int port;
-    private ProtocolFactory factory;
-    private List<Integer> ids = new ArrayList<>();
-    private Rpc rpc;
+public class MultiCmp extends AbyWrapper {
     private static final Logger LOG = LoggerFactory.getLogger(MultiCmp.class);
 
-    public MultiCmp(Rpc rpc, int me, int num, String address, int port, ProtocolFactory factory) {
-        this.rpc = rpc;
-        this.me = me;
-        this.num = num;
-        this.address = address;
-        this.port = port;
-        this.factory = factory;
-        ids.add(me);
+    public MultiCmp(List<OpenHuFuRpc> rpcs, int me, List<Integer> ids, String address, int port) {
+        super(rpcs, me, ids, address, port);
     }
 
-    int runMultiCmp(int taskId, int val) throws ProtocolException {
-        LOG.info("run CMP: {}, {}, {}", me, taskId, val);
+    @Override
+    public Object run(long taskId, List<Integer> parties, Object input, OperatorType op) throws ProtocolException {
         if (me == 0) {
-            int curMax = val;
+            List<Integer> abyIds = ImmutableList.of(0, 1);
+            int curMax = (int)input;
             for (int id = 1; id < num; id++) {
-                int curPort = this.port + id;
+                int curPort = this.port;
                 ProtocolExecutor aby = factory.create(
                     OwnerInfo.newBuilder().setEndpoint(address + ":" + curPort).setId(me).build(), ProtocolType.ABY);
-                List<byte[]> input = ImmutableList.of(OpenHuFuCodec.encodeInt(curMax), OpenHuFuCodec.encodeInt(0));
-                List<byte[]> tmp = (List<byte[]>) aby.run(taskId, ids, input, OperatorType.MAX, ColumnType.INT, 
-                    this.address, curPort, id != num - 1);
-                curMax = OpenHuFuCodec.decodeInt(tmp.get(0));
-                LOG.info("round {}: {}", id, curMax);
+                List<byte[]> abyInputs = ImmutableList.of(OpenHuFuCodec.encodeInt(curMax), OpenHuFuCodec.encodeInt(0));
+                List<byte[]> abyOutputs = (List<byte[]>) aby.run(taskId, abyIds, abyInputs, op, ColumnType.INT, address, curPort, id != num - 1, (int)taskId * 100 + id);
+                curMax = OpenHuFuCodec.decodeInt(abyOutputs.get(0));
+                LOG.info("round {}: {}", id, Integer.toHexString(curMax));
             }
             return curMax;
         }
+        List<Integer> abyIds = ImmutableList.of(1, 0);
         if (me == 1 && num == 2) {
             ProtocolExecutor aby = factory.create(
                 OwnerInfo.newBuilder().setEndpoint(address + ":" + port).setId(me).build(), ProtocolType.ABY);
-            List<byte[]> input = ImmutableList.of(OpenHuFuCodec.encodeInt(0), OpenHuFuCodec.encodeInt(val));
-            List<byte[]> tmp = (List<byte[]>) aby.run(taskId, ids, input, OperatorType.MAX, ColumnType.INT, 
-                this.address, this.port, false);
-            int ret = OpenHuFuCodec.decodeInt(tmp.get(0));
+            List<byte[]> abyInputs = ImmutableList.of(OpenHuFuCodec.encodeInt(0), OpenHuFuCodec.encodeInt((int)input));
+            List<byte[]> abyOutputs = (List<byte[]>) aby.run(taskId, abyIds, abyInputs, op, ColumnType.INT, address, port, false, (int)taskId * 100 + me);
+            int ret = OpenHuFuCodec.decodeInt(abyOutputs.get(0));
             return ret;
         }
-        if (me == 1) {
-            // TODO: multiparty
-            // ProtocolExecutor aby = factory.create(
-            //     OwnerInfo.newBuilder().setEndpoint(address + port).setId(me).build(), ProtocolType.ABY);
+
+        int val1 = 0, val2 = (int)input;
+        if (me != 1) {
+            List<byte[]> recv = rpcRecv(taskId, me - 1, me - 1);
+            val1 = OpenHuFuCodec.decodeInt(recv.get(0));
+            LOG.info("{} recv {}", me, Integer.toHexString(val1));
         }
-        // TODO: multiparty
+        ProtocolExecutor aby = factory.create(
+            OwnerInfo.newBuilder().setEndpoint(address + ":" + port).setId(me).build(), ProtocolType.ABY);
+        List<byte[]> abyInputs = ImmutableList.of(OpenHuFuCodec.encodeInt(val1), OpenHuFuCodec.encodeInt(val2));
+        List<byte[]> abyOutputs = (List<byte[]>) aby.run(taskId, abyIds, abyInputs, op, ColumnType.INT, address, port, me != num - 1, (int)taskId * 100 + me);
+        val1 = OpenHuFuCodec.decodeInt(abyOutputs.get(0));
+        if (me != num - 1) {
+            byte[] send = OpenHuFuCodec.encodeInt(val1);
+            rpcSend(taskId, me, me + 1, ImmutableList.of(send));
+            LOG.info("{} send {}", me, Integer.toHexString(val1));
+        }
         return 0;
     }
 }
--- mpc/src/test/java/com/hufudb/openhufu/mpc/lib/aby/MultiCmpTest.java
+++ mpc/src/test/java/com/hufudb/openhufu/mpc/lib/aby/MultiCmpTest.java
@@ -1,34 +1,25 @@
 package com.hufudb.openhufu.mpc.lib.aby;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.lenient;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import com.hufudb.openhufu.rpc.Party;
-import com.hufudb.openhufu.rpc.Rpc;
 import com.hufudb.openhufu.rpc.grpc.OpenHuFuOwnerInfo;
 import com.hufudb.openhufu.rpc.grpc.OpenHuFuRpcManager;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
-import com.hufudb.openhufu.mpc.lib.LibraryLoader;
 import io.grpc.Channel;
-import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.Grpc;
 import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
-
-import java.util.Map;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import com.hufudb.openhufu.mpc.ProtocolFactory;
-import com.hufudb.openhufu.mpc.ProtocolType;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import com.hufudb.openhufu.proto.OpenHuFuPlan.OperatorType;
 
 import com.hufudb.openhufu.rpc.grpc.OpenHuFuRpc;
 import org.junit.After;
@@ -45,35 +36,41 @@ public class MultiCmpTest {
     public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
     private static final Logger LOG = LoggerFactory.getLogger(MultiCmpTest.class);
 
-    private ArrayList<OpenHuFuRpc> rpcs = new ArrayList<>();
-    private ArrayList<MultiCmp> cmps = new ArrayList<>();
-    private int partyNum = 2;
+    private List<OpenHuFuRpc> rpcs = new ArrayList<>();
+    private List<MultiCmp> cmps = new ArrayList<>();
+    private List<Integer> ids = new ArrayList<>();
+    private int partyNum = 10;
 
     @Before
     public void setUp() throws IOException {
-        String openhufuRoot = System.getenv("OPENHUFU_ROOT");
-        Path libDir = Paths.get(openhufuRoot, "lib");
-        Map<ProtocolType, ProtocolFactory> factories =
-            LibraryLoader.loadProtocolLibrary(libDir.toString());
-        ProtocolFactory factory = factories.get(ProtocolType.ABY);
         String[] ownerNames = new String[partyNum];
         List<Party> parties = new ArrayList<>();
         List<Channel> channels = new ArrayList<>();
         for (int p = 0; p < partyNum; p++) {
+            ids.add(2 * p + 1);
             ownerNames[p] = InProcessServerBuilder.generateName();
-            parties.add(new OpenHuFuOwnerInfo(p, ownerNames[p]));
-            channels.add(grpcCleanup.register(InProcessChannelBuilder.forName(ownerNames[p]).directExecutor().build()));
+            parties.add(new OpenHuFuOwnerInfo(ids.get(p), ownerNames[p]));
+            channels.add(grpcCleanup.register(
+                Grpc.newChannelBuilder("127.0.0.1:" + (7000 + p), InsecureChannelCredentials.create()).build()
+            ));
         }
         OpenHuFuRpcManager manager = new OpenHuFuRpcManager(parties, channels);
         for (int p = 0; p < partyNum; p++) {
-            rpcs.add((OpenHuFuRpc) manager.getRpc(p));
-            grpcCleanup.register(InProcessServerBuilder.forName(ownerNames[p]).directExecutor()
-                .addService(rpcs.get(p).getgRpcService()).build().start());
+            rpcs.add((OpenHuFuRpc) manager.getRpc(ids.get(p)));
+            Server s = Grpc.newServerBuilderForPort(7000 + p, InsecureServerCredentials.create()).
+                directExecutor().addService(rpcs.get(p).getgRpcService()).build();
+            s.start();
+            
+            grpcCleanup.register(s);
             rpcs.get(p).connect();
         }
         for (int p = 0; p < partyNum; p++) {
-            MultiCmp c = new MultiCmp(rpcs.get(p), p, partyNum, "127.0.0.1", 6000 + p, factory);
+            MultiCmp c = new MultiCmp(rpcs, p, ids, "127.0.0.1", 8000);
             cmps.add(c);
+        }
+
+        for (int p = 0; p < partyNum; p++) {
+            LOG.info("rpcs[{}]: {}, {}, {}", p, ownerNames[p], rpcs.get(p).ownParty().getPartyId(), channels.get(p));
         }
     }
 
@@ -92,6 +89,8 @@ public class MultiCmpTest {
         int[] inputs = new int[partyNum];
         inputs[0] = 30;
         inputs[1] = 29;
+        inputs[2] = 229;
+        inputs[3] = 129;
 
         ArrayList<Future<Integer>> results = new ArrayList<>();
         for (int p = 0; p < partyNum; p++) {
@@ -101,17 +100,57 @@ public class MultiCmpTest {
                 @Override
                 public Integer call() throws Exception {
                     LOG.info("input = {}", input);
-                    int ret = cmp.runMultiCmp(taskId, input);
+                    int ret = (int)cmp.run(taskId, ids, input, OperatorType.MAX);
                     return ret;
                 }
             });
             results.add(res);
         }
         for (int p = 0; p < partyNum; p++) {
-            int res = (int)(results.get(0).get());
+            int res = (int)(results.get(p).get());
             if (p == 0) {
-                assertEquals(res, 30);
+                assertEquals(res, 229);
             }
         }
+    }
+    
+    @Test
+    public void testMultiCmpMultiTimes() throws Exception {
+        ExecutorService service = Executors.newFixedThreadPool(partyNum);
+
+        int[] inputs = new int[partyNum];
+
+        for (int taskId = 0; taskId < 50; taskId++) {
+            int ans = 0;
+            for (int i = 0; i < partyNum; i++) {
+                inputs[i] = (i * 10 + taskId) % (partyNum + 7);
+                if (inputs[i] > ans) {
+                    ans = inputs[i];
+                }
+            }
+            ArrayList<Future<Integer>> results = new ArrayList<>();
+            for (int p = 0; p < partyNum; p++) {
+                MultiCmp cmp = cmps.get(p);
+                int input = inputs[p];
+                long tid = taskId;
+                Future<Integer> res = service.submit(new Callable<Integer>() {
+                    @Override
+                    public Integer call() throws Exception {
+                        LOG.info("cmp input = {}", input);
+                        int ret = (int)cmp.run(tid, ids, input, OperatorType.MAX);
+                        return ret;
+                    }
+                });
+                results.add(res);
+            }
+            for (int p = 0; p < partyNum; p++) {
+                int res = (int)(results.get(p).get());
+                if (p == 0) {
+                    LOG.info("taskId = {}", taskId);
+                    assertEquals(res, ans);
+                }
+            }
+        }
+        
     }
 }
