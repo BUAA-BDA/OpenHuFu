e1b1d79c0ee0ee1e8e7c600acd391e0719db8c78
qvlehao <2775257495@qq.com>
2023-11-22 13:05:10 +0800

    wxy fix

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY ./release/bin ./bin
 COPY ./release/adapter ./adapter
 ADD ./release/*.sh ./
 ENTRYPOINT sh -c 'chmod 777 ./application.sh' &&\
-           sh -c 'bash ./application.sh start ./config/spatialOwner.json ./config/tasks-KNN.json' &&\
+           sh -c 'bash ./application.sh start ./config/spatialOwner1.json ./config/tasks-KNN.json' &&\
            sh -c 'tail -f /dev/null'
 EXPOSE 12345
---
+++ scripts/wxy/prepare.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+config_modify() {
+  sed -i 's/db1.sql/db2.sql/g' ./k8s/database/postgis.yaml
+  sed -i 's/computer1/computer2/g' ./mpc-chart/values.yaml
+  sed -i 's/192.168.0.101/192.168.0.104/g' ./mpc-chart/values.yaml
+  sed -i 's/spatialOwner1.json/spatialOwner2.json/g' ./Dockerfile
+}
+
+update_version() {
+  sed -i 's/openhufu-server:1.0/openhufu-server:'$1'/g' ./scripts/build/image.sh
+  sed -i 's/tag: "1.0"/tag: "'$1'"/g' ./mpc-chart/values.yaml
+}
+
+setup_pg() {
+  docker pull postgis/postgis:latest
+  docker tag postgis/postgis:latest docker.oa.com:5000/mpc/postgis/postgis:1.6
+  docker push docker.oa.com:5000/mpc/postgis/postgis:1.6
+  kubectl apply -f ./k8s/database/postgis-storage.yaml
+  kubectl apply -f ./k8s/database/postgis.yaml
+}
+
+prepare_hufu() {
+  ./scripts/build/image.sh
+}
+
+usage() {
+  echo "usage: ./prepare.sh <party_id> <hufu_version>"
+}
+
+if [ $# -eq 2 ]
+then
+  if [ $1 == "2" ]
+  then
+    config_modify
+  fi
+  update_version $2
+else
+  usage
+fi
+setup_pg
+prepare_hufu
