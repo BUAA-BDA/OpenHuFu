7981a8130165b214f023ef108b6531ee15647dee
qvlehao <2775257495@qq.com>
2024-03-19 09:26:19 +0800

    wxy vectorknn add

--- scripts/wxy/prepare.sh
+++ scripts/wxy/prepare.sh
@@ -17,7 +17,8 @@ update_version() {
 setup_pg() {
   docker pull postgis/postgis:latest
   docker tag postgis/postgis:latest docker.oa.com:5000/mpc/postgis/postgis:1.6
-  docker push docker.oa.com:5000/mpc/postgis/postgis:1.6
+  docker build -f .k8s/database/Dockerfile -t docker.oa.com:5000/mpc/pgvector/pgvector:1.6 .
+  docker push docker.oa.com:5000/mpc/pgvector/pgvector:1.6
   kubectl apply -f ./k8s/database/postgis-storage.yaml
   kubectl apply -f ./k8s/database/postgis.yaml
 }
